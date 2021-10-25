0ea5d53ce64bb182e6c176dfccb56f3a1747b911
MekonenWase <mekonen.wase@gmail.com>
2021-10-26 01:44:58 +0300

    home-work25/10

--- node-19-10-2021/mead_nath_weather_location_web_server/src/server.js
+++ node-19-10-2021/mead_nath_weather_location_web_server/src/server.js
@@ -2,15 +2,43 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const axios = require("axios");
-const PORT = 1000;
-const publicPath = path.join(__dirname, "public");
+const PORT = 8080;
+const publicPath = path.join(__dirname,"..","public");
+const hbs = require('hbs'),
+viewsPath = path.join(__dirname,"..","templetes","views"),
+partialsPath =path.join(__dirname,"..","templetes","partials");
+
+app.set("view engine","hbs");
+
+app.set("views",viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicPath));
+
+app.get("/",(req,res)=>{
+  res.render("index");
+});
+
+app.get("/about",(req,res)=>{
+  res.render("about");
+});
+
+app.get("/help",(req,res)=>{
+  res.render("help");
+});
+
+app.get("/error",(req,res)=>{
+  res.render("error");
+});
+
+app.get("/location",(req,res)=>{
+  res.render("location");
+});
+
 app.get("/city", (req, res) => {
   const apiKey = `3ea2acb526183189904890dbad0452df`;
   const cityName = `${req.query.city}`;
   const apiUrL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-
   axios
     .get(apiUrL)
     .then(function (response) {
