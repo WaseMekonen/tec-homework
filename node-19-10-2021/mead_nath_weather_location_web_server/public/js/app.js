d7f18b54393e9c8439530f1dcf31637aa3fa3ffd
MekonenWase <mekonen.wase@gmail.com>
2021-10-27 23:26:44 +0300

    fix site

--- node-19-10-2021/mead_nath_weather_location_web_server/public/js/app.js
+++ node-19-10-2021/mead_nath_weather_location_web_server/public/js/app.js
@@ -1,1 +1,33 @@
 console.log("check this on the browser console");
+
+dataDisplay = document.getElementById("location");
+const cityInput = document.getElementById("city-input");
+document.getElementById("form").addEventListener("submit", cityDisplay);
+
+function cityDisplay(e) {
+    e.preventDefault();
+  const city = cityInput.value;
+
+  axios
+    .get(`/${city}`)
+    .then(function (response) {
+      console.log(response);
+      console.log("it's good");
+      if (response.status === 200) {
+        console.log(response.data);
+            cityInput.innerHTML=`City: ${response.data.name},
+            Longitude: ${response.data.coord.lon},
+            latitude:${response.data.coord.lat}`
+        //   res.send(
+        //     `City: ${response.data.name},
+        //      Longitude: ${response.data.coord.lon},
+        //     Latitude:${response.data.coord.lat}`
+        //   );
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log("request got error");
+      res.render("error");
+    });
+}
--- node-19-10-2021/mead_nath_weather_location_web_server/src/server.js
+++ node-19-10-2021/mead_nath_weather_location_web_server/src/server.js
@@ -3,60 +3,65 @@ const path = require("path");
 const app = express();
 const axios = require("axios");
 const PORT = 8080;
-const publicPath = path.join(__dirname,"..","public");
-const hbs = require('hbs'),
-viewsPath = path.join(__dirname,"..","templetes","views"),
-partialsPath =path.join(__dirname,"..","templetes","partials");
+const publicPath = path.join(__dirname, "..", "public");
 
-app.set("view engine","hbs");
+const hbs = require("hbs"),
+  viewsPath = path.join(__dirname, "..", "templetes", "views"),
+  partialsPath = path.join(__dirname, "..", "templetes", "partials");
 
-app.set("views",viewsPath);
+app.set("view engine", "hbs");
+
+app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicPath));
 
-app.get("/",(req,res)=>{
-  res.render("index");
+app.get("/", (req, res) => {
+  res.render("index",{title:"Home"});
 });
 
-app.get("/about",(req,res)=>{
-  res.render("about");
+app.get("/about", (req, res) => {
+  res.render("index",{title:"about",box:true});
 });
 
-app.get("/help",(req,res)=>{
+app.get("/help", (req, res) => {
   res.render("help");
 });
 
-app.get("/error",(req,res)=>{
+app.get("/error", (req, res) => {
   res.render("error");
 });
 
-app.get("/location",(req,res)=>{
+app.get("/location", (req, res) => {
   res.render("location");
 });
 
-app.get("/city", (req, res) => {
+app.get("/:city", (req, res) => {
   const apiKey = `3ea2acb526183189904890dbad0452df`;
-  const cityName = `${req.query.city}`;
-  const apiUrL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  const cityName = `${req.params}`;
+  const apiUrL = `http://api.openweathermap.org/data/2.5/weather?q=lod&appid=${apiKey}`;
   axios
     .get(apiUrL)
     .then(function (response) {
       console.log(response);
       console.log("it's good");
       if (response.status === 200) {
+        console.log("its work");
         console.log(response.data);
         res.send(
-          `name: ${response.data.name}, longitude: ${response.data.coord.lon}, latitude:${response.data.coord.lat}`
+          `City: ${response.data.name}, 
+           Longitude: ${response.data.coord.lon}, 
+          Latitude:${response.data.coord.lat}`
         );
       }
     })
     .catch(function (error) {
       // handle error
       console.log("request got error");
-      console.log(error);
+      res.render("error");
     });
 });
+
 app.listen(PORT, () => {
   console.log(`App is listening to : ${PORT}`);
 });
