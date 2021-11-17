b10c2ce15ac9ae46715334a01b475ca8c2e53cd5
MekonenWase <mekonen.wase@gmail.com>
2021-11-17 15:56:12 +0200

    h.w16/11

--- next_generation_js_15_11_2021/homeWork.js
+++ next_generation_js_15_11_2021/homeWork.js
@@ -5,18 +5,18 @@
 
 // 2:
 
-const array1 = [];
+// const array1 = [];
 
-for(let i=0; i<1000; i++){
-    array1.push(i)
-}
+// for(let i=0; i<1000; i++){
+//     array1.push(i)
+// }
 
-const newArray = array1.map(number=> (Math.sqrt(number)))
-console.log(newArray);
+// const newArray = array1.map(number=> (Math.sqrt(number)))
+// console.log(newArray);
 
 // 3:
 
-const arrayOfnum = [11,23,4,566,53,932,3,22];
+const arrayOfnum = [11,23,4,566,53,932,3,22,11,90,78,43,12];
 
 const arrayOfOddNumbers = arrayOfnum.filter(num=> num%2==0);
 console.log(arrayOfOddNumbers);
